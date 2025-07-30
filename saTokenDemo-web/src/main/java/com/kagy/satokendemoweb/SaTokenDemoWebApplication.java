e8452fb08e86ab56ffd4a5756ced1f7a68d31c69
lzt <490845291@qq.com>
2025-07-30 17:47:47 +0800

    init1

---
+++ saTokenDemo-web/src/main/java/com/kagy/satokendemoweb/SaTokenDemoWebApplication.java
@@ -0,0 +1,13 @@
+package com.kagy.satokendemoweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaTokenDemoWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaTokenDemoWebApplication.class, args);
+    }
+
+}
---
+++ saTokenDemo-web/src/test/java/com/kagy/satokendemoweb/SaTokenDemoWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kagy.satokendemoweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaTokenDemoWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
